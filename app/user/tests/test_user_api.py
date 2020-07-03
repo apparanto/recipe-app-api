813e826114bcf8ed06afb225a68a686cac6a7338
Robert Portier <robert.portier@apparanto.com>
2020-07-03 16:28:05 +0200

    Fix unit test for creating user with a too short password; the name parameter was not passed, so the api always returns a BAD REQUEST

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -44,7 +44,7 @@ class PublicUserApiTests(TestCase):
 
     def test_password_too_short(self):
         '''Test that the password must be more than 5 characters'''
-        payload = {'email': 'test@apparanto.com', 'password': 'Tst'}
+        payload = {'email': 'test@apparanto.com', 'name': 'Test 1234', 'password': 'Tst'}
 
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
